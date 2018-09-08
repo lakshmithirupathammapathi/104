6b274b119e341b32af3b8bcbb4f90fd7a80557bc
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-09-08 11:02:13 +0530

    Create 104.java

---
+++ 104.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int base , exponent;
+		base=sc.nextInt();
+		exponent=sc.nextInt();
+
+        long result = 1;
+
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println(result);
+
+	}
+}
